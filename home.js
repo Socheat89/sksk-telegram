0a7d51c8ae48d91085467f7b1496e7a2ec5c65a8
Socheat89 <105296904+Socheat89@users.noreply.github.com>
2024-11-27 11:46:43 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,15 +1,1 @@
-function navigate(destination) {
-    switch (destination) {
-        case 'store':
-            alert("Navigating to SK STORE");
-            break;
-        case 'shop':
-            alert("Navigating to SK SHOP");
-            break;
-        case 'map':
-            alert("Navigating to MAP");
-            break;
-        default:
-            alert("Unknown destination");
-    }
-}
+
